45302cb0e1ac529b58b9aac49e7e375a60d07194
Oleksiy <58302277+EternalJew@users.noreply.github.com>
2021-07-09 14:44:12 +0300

    addPlayerController
    integrate move and swipe

---
+++ EndLessRunner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Move dir.z
+    private CharacterController controller;
+    private Vector3 dir;
+    [SerializeField] private int speed;
+    //SwipeController
+    private int lineToMove = 1;
+    public float lineDistance = 4;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        if(SwipeController.swipeRight)
+        {
+            if(lineToMove < 2)
+            {
+                lineToMove++;
+            }
+        }
+        if(SwipeController.swipeLeft)
+        {
+            if(lineToMove > 0)
+            {
+                lineToMove--;
+            }
+        }
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        if(lineToMove == 0)
+            targetPosition += Vector3.left * lineDistance;
+        else if(lineToMove == 2)
+            targetPosition += Vector3.right * lineDistance;
+
+        transform.position = targetPosition;
+    }
+    void FixedUpdate()
+    {
+        dir.z = speed;
+        controller.Move(dir * Time.fixedDeltaTime);
+    }
+}
